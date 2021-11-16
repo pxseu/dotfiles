49d357d98e9c539de8086984680def305e7e45d0
pxseu <57842793+pxseu@users.noreply.github.com>
2021-11-16 12:44:09 +0100

    feat: update dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
-
 #!/bin/bash
+
+echo "Installing deps"
+yes | sudo pacman -S --needed neovim zsh git kitty base-devel
+
 cd "$(dirname "$0")"
+echo "Installing ZSH"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
+for dir in */
+do
+    dir=${dir%*/}
+    echo "> Now stowing: ${dir##*/}"
+    stow ${dir##*/}
+done
